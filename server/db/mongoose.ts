d96383cad10acbf5acb330289b3c2fa4e60524b7
Shikamu <tomy4ever@gmail.com>
2018-06-09 17:10:56 +0200

    setup app for heroku

--- server/db/mongoose.ts
+++ server/db/mongoose.ts
@@ -3,7 +3,7 @@ import mongoose = require("mongoose");
 export namespace db
 {
     (<any>mongoose).Promise = global.Promise;
-    export var connection: mongoose.Connection = mongoose.createConnection("mongodb://localhost:27017/TodoApp");
+    export var connection: mongoose.Connection = mongoose.createConnection(process.env.MONGODB_URI ||"mongodb://localhost:27017/TodoApp");
     export var ObjectID = mongoose.Types.ObjectId;
     console.log("created connection");
 }
